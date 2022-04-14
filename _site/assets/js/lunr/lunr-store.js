653c44ad7e0bc889f102ff8f4705329d4b4808fb
junkyul <junkyulee82@gmail.com>
2022-04-14 07:32:56 -0400

    update site files (github ignores this)

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,31 +1,25 @@
 var store = [{
-        "title": "Results Board",
-        "excerpt":"PR                  Solver       20 secs       20 mins       1 hour                       bp       11474.22904 (0/138)       3017.99606 (0/138)       2902.2579 (0/138)                 ai       8.32295 (0/138)       16.5076 (0/138)       14.1457 (0/138)           ","categories": [],
+        "title": "Results",
+        "excerpt":"TBD  ","categories": [],
         "tags": [],
         "url": "/test-page1/leaderboard/results-board/",
         "teaser": null
       },{
-        "title": "Announcements: test1",
-        "excerpt":"The competition will run from June 15 to July 15. Winners will be announced on July 19.  ","categories": ["due dates"],
+        "title": "Announcements: news",
+        "excerpt":"In April, this site will be open to public and we will send call for participations.  ","categories": ["due dates"],
         "tags": [],
-        "url": "/test-page1/due%20dates/announcement/",
+        "url": "/test-page1/due%20dates/news/",
         "teaser": null
       },{
-        "title": "Welcome Message",
-        "excerpt":"We are pleased to announce an upcoming evaluation of probabilistic approximate inference algorithms, as part of the UAI 2014 conference. All researchers working on inference in graphical models and statistical relational models are encouraged to participate.  ","categories": ["general"],
+        "title": "Hello World!",
+        "excerpt":" ","categories": ["general"],
         "tags": [],
-        "url": "/test-page1/general/welcome/",
+        "url": "/test-page1/general/hello/",
         "teaser": null
       },{
-        "title": "Time 20 seconds",
-        "excerpt":"        Lower scores are better            Baseline is asynchronous BP at same time frame so even if absolute performance improves as more time is given, numbers could go up                      Group Name       PR       MAR       MPE                       agelfand       37.3736       -       -                 ai       9.9057       0.8486       -                 ai-lp       0.8550       0.5916       -                 AsynchBP       131.9813       1.5056       -0.0753                 daoopt       -       -       -6.1314                 daoopt+jglp       -       -       -6.1364                 daoopt+mm       -       -       -6.1298                 daoopt-br       -       -       -6.1314                 dfbbve       -       -       -4.5244                 dfbbvemcs       -       -       -6.0518           ","categories": [],
+        "title": "Evaluation Criteria",
+        "excerpt":"TBD  ","categories": [],
         "tags": [],
-        "url": "/test-page1/summary/time-20sec/",
-        "teaser": null
-      },{
-        "title": "Winners",
-        "excerpt":"                       20 secs       20 mins       1 hour                       PR                                         First       ai       edbp       ai                 Second       edbp       ai       edbp                 MAR                                         First       ai       ai       ai                 Second       edbp       edbp       edbp                 MPE                                         First       Robin       Proteus       Proteus                 Second       Proteus       daoopt       daoopt                 Third       IncTb       Robin       Robin                 MMAP                                         First       daoopt       daoopt       daoopt                 Second       wmbjg       wmbjg       ibm-mmap-jg           ","categories": [],
-        "tags": [],
-        "url": "/test-page1/summary/winners/",
+        "url": "/test-page1/summary/Evaluation-Criteria/",
         "teaser": null
       }]
